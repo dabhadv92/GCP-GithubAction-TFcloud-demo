6aed012b9e3e86dbf713b0d99685581e3321609b
Vivek <dabhadev92@gmail.com>
2024-06-06 18:46:42 +0530

    first commmit

---
+++ variable.tf
@@ -0,0 +1,19 @@
+
+
+
+variable "vpc-name" {
+  type = string
+  default = "vpc-tfcloud-pipeline"
+}
+
+variable "subnet_name" {
+  type = string
+  default = "subnet-tfcloud-pipeline"
+}
+
+variable "ip_cidr_range" {
+  type = string
+  default = "192.168.140.0/24"
+}
+
+
