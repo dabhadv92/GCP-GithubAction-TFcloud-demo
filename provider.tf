979d8588af7c65f629477e647bfdafa4a0baf34d
Vivek <dabhadev92@gmail.com>
2024-06-16 22:12:26 +0530

    First Commit

--- provider.tf
+++ provider.tf
@@ -3,29 +3,29 @@ terraform {
   required_version = ">= 1.0.0"
   required_providers {
     google = {
-        version = "5.27.0"
-        source = "hashicorp/google"
+      version = "5.27.0"
+      source  = "hashicorp/google"
     }
   }
-  
+
   cloud {
     organization = "BDX"
     workspaces {
       name = "GCP-GithubAction-TFcloud-demo"
     }
   }
-  
-  
+
+
 }
 
 provider "google" {
-    project = "terraform-practice-421214"
-    zone = "asia-south1-a"
-    region = "asia-south1"
-    #credentials = "terraform-practice-key.json"
-  
+  project = "terraform-practice-421214"
+  zone    = "asia-south1-a"
+  region  = "asia-south1"
+  #credentials = "terraform-practice-key.json"
+
 }
 
 
 
-  
+
--- variable.tf
+++ variable.tf
@@ -2,17 +2,17 @@
 
 
 variable "vpc-name" {
-  type = string
+  type    = string
   default = "vpc-tfcloud-pipeline"
 }
 
 variable "subnet_name" {
-  type = string
+  type    = string
   default = "subnet-tfcloud-pipeline"
 }
 
 variable "ip_cidr_range" {
-  type = string
+  type    = string
   default = "192.168.140.0/24"
 }
 
